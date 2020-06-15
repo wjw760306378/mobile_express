4496906123b0677377e8e010ab374ce6b386c851
gaoqijun <gaoqijun@kedacom.com>
2020-06-15 18:32:37 +0800

    提交空文件夹

--- public/javascripts/getLocalIp.js
+++ public/javascripts/getLocalIp.js
@@ -1,7 +1,9 @@
 module.exports = function getLocalIp() {
 	var interfaces = require('os').networkInterfaces();
+	console.log('interfaces',interfaces)
 	for(var devName in interfaces){
 		var iface = interfaces[devName];
+		console.log('iface',iface)
 		for(var i=0;i<iface.length;i++){
 			var alias = iface[i];
 			if(alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal){
